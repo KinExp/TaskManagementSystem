25472d5a8d89f6f34d51a913a1ab24c5c346256d
KinExp <kinexp.08@yandex.ru>
2026-01-09 20:12:04 +0500

    Add validation attributes to CreateTaskDto

--- TaskManagementSystem/Application/DTOs/CreateTaskDto.cs
+++ TaskManagementSystem/Application/DTOs/CreateTaskDto.cs
@@ -1,12 +1,20 @@
-﻿using TaskManagement.Domain.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagement.Domain.Enums;
 
 namespace TaskManagement.Application.DTOs
 {
     public class CreateTaskDto
     {
+        [Required]
+        [MaxLength(200)]
         public string Title { get; init; } = null!;
+
+        [MaxLength(1000)]
         public string? Description { get; init; }
+
+        [Required]
         public TaskPriority Priority { get; init; } = TaskPriority.Medium;
+
         public DateTime? Deadline { get; init; }
     }
 }
