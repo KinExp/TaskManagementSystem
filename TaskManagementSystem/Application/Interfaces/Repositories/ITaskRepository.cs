c4d01c2c29a0d1b63aecc10e2a56efe212ef10dc
KinExp <kinexp.08@yandex.ru>
2026-01-09 13:51:52 +0500

    Fix nullable return type in ITaskRepository.GetByIdAsync

--- TaskManagementSystem/Application/Interfaces/Repositories/ITaskRepository.cs
+++ TaskManagementSystem/Application/Interfaces/Repositories/ITaskRepository.cs
@@ -5,7 +5,7 @@ namespace TaskManagement.Application.Interfaces.Repositories
     public interface ITaskRepository
     {
         Task AddAsync(TaskItem task);
-        Task<TaskItem> GetByIdAsync(Guid id);
+        Task<TaskItem?> GetByIdAsync(Guid id);
         Task<IReadOnlyList<TaskItem>> GetByUserAsync(Guid userId);
         Task SaveChangesAsync();
     }
